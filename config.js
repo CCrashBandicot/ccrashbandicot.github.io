a79938bceb5bb9352db8649b44e2a54ca2234f59
Abdel bendouil <ccrashbandicot@hotmail.fr>
2015-10-09 05:38:31 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+var file_list = [
+ ["/home/crash",1,"text","2015 Jun 22 17:00"],
+ ["/home/crash",1,"c0de","2015 Jun 22 17:00"],
+
+
+ ["/home/crash", 0,"README", "2015 Jun 22 17:00","Meu sitezim v0.0.1<br/>",""],
+ ["/home/crash", 0,"links.txt", "2015 Jun 22 17:00","Alguns links legais :D<br/>",""],
+
+ ["/home/crash/text", 0, "read_file_tutorial.txt", "2015 Ter Jun 23 15:40", "", "https://raw.githubusercontent.com/P0cL4bs/Shellcodes/master/read_file_tutorial.txt"]
+
+];
+
+var dirs = ['/home/crash', '/home/crash/text', '/home/crash/c0de'];
+var pwd = '/home/crash';
+
