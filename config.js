7bcc70e5bf200c8d9e5e27b958c4758a784f5dae
Abdel bendouil <ccrashbandicot@hotmail.fr>
2015-10-13 03:34:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 var file_list = [
- ["/home/crash",1,"text","2015 Jun 22 17:00"],
+ ["/home/crash",1,"txt","2015 Jun 22 17:00"],
  ["/home/crash",1,"c0de","2015 Jun 22 17:00"],
 
 
- ["/home/crash", 0,"README", "2015 Jun 22 17:00","Website Hosted by Github",""],
- ["/home/crash", 0,"links.txt", "2015 Jun 22 17:00","https://github.com/CCrashBandicot<br>twitter.com/DosPerl<br>pastebin.com/u/CrashBandicot",""],
+ ["/home/crash", 0,"README", "2015 Jun 22 17:00","Website Hosted by Github<br>",""],
+ ["/home/crash", 0,"links.txt", "2015 Jun 22 17:00","https://github.com/CCrashBandicot<br>twitter.com/DosPerl<br>pastebin.com/u/CrashBandicot<br>",""],
 
- ["/home/crash/text", 0, "read_file_tutorial.txt", "2015 Ter Jun 23 15:40", "", "https://www.github.com/CCrashBandicot"]
+ ["/home/crash/txt", 0, "read_file_tutorial.txt", "2015 Ter Jun 23 15:40", "", "https://www.github.com/CCrashBandicot"]
 
 ];
 
-var dirs = ['/home/crash', '/home/crash/text', '/home/crash/c0de'];
+var dirs = ['/home/crash', '/home/crash/txt', '/home/crash/c0de'];
 var pwd = '/home/crash';
 
