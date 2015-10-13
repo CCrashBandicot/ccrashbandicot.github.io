f8846aa3d3d70d142de4d075db65e9d7c0a91370
Abdel bendouil <ccrashbandicot@hotmail.fr>
2015-10-13 03:32:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var file_list = [
  ["/home/crash",1,"c0de","2015 Jun 22 17:00"],
 
 
- ["/home/crash", 0,"README", "2015 Jun 22 17:00","Meu sitezim v0.0.1<br/>",""],
- ["/home/crash", 0,"links.txt", "2015 Jun 22 17:00","Alguns links legais :D<br/>",""],
+ ["/home/crash", 0,"README", "2015 Jun 22 17:00","Website Hosted by Github",""],
+ ["/home/crash", 0,"links.txt", "2015 Jun 22 17:00","https://github.com/CCrashBandicot<br>twitter.com/DosPerl<br>pastebin.com/u/CrashBandicot",""],
 
- ["/home/crash/text", 0, "read_file_tutorial.txt", "2015 Ter Jun 23 15:40", "", "https://raw.githubusercontent.com/P0cL4bs/Shellcodes/master/read_file_tutorial.txt"]
+ ["/home/crash/text", 0, "read_file_tutorial.txt", "2015 Ter Jun 23 15:40", "", "https://www.github.com/CCrashBandicot"]
 
 ];
 
